b639e2aecd5dc36900f345dfa04f98e373132563
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-06-24 21:57:55 +0100

    beginnings of JWT and guest access levels + change to a findbyid route

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,17 @@ import LoginPage from './pages/LoginPage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import {ACCESS_LEVEL_GUEST} from "./config/global_constants"
 
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import {useState} from "react";
+
+if(typeof localStorage.accessLevel === "undefined")
+{
+    localStorage.name = "GUEST"
+    localStorage.accessLevel = ACCESS_LEVEL_GUEST
+    localStorage.token = null
+}
 
 function App() {
   return (
@@ -15,7 +24,7 @@ function App() {
         <Routes>
             <Route exact path="/" element={<ContentPage />}/>
             <Route exact path="/profile" element={<ProfilePage />}/>
-            <Route exact path="/product/:id" element={<ProductPage />}/>
+            <Route exact path="/product/:_id" element={<ProductPage />}/>
             <Route exact path="/login" element={<LoginPage />}/>
             <Route exact path="/register" element={<RegisterPage />}/>
 
