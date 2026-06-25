07221f2d9fadf2598cc836f53aa1c76e505e6395
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-06-26 00:48:45 +0100

    made ContentPage/App pass the search term effectively from navbar's searchbar to the LoadAllProducts comp

--- client/src/pages/ContentPage.jsx
+++ client/src/pages/ContentPage.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from '../components/NavBar';
 import Searchbar from "../components/Searchbar";
 import LoadAllProducts from "../components/LoadAllProducts";
 import Eggon from "../components/Eggon";
 
 export default function ContentPage() {
+
+    const [search, setSearch] = useState("");
+
     return (
         <div className="ContentPage">
-            <NavBar />
-            <hr/>
+            <NavBar
+                search={search}
+                setSearch={setSearch}
+            />
             <Eggon />
-            <Searchbar />
-            <LoadAllProducts />
+            <LoadAllProducts
+                search={search}
+            />
         </div>
     )
 }
