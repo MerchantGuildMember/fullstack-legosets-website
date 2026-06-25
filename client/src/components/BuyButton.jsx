b362d778e3f67dfbb902ef927f7e652fdd5ee51e
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-06-25 11:23:20 +0100

    created a basic button for the later buy button

---
+++ client/src/components/BuyButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BuyButton(props) {
+
+    return (
+        <div className="buyButton">
+            <button>Buy Now</button>
+        </div>
+    )
+}
