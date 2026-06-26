643a196e4a928f16d67ff851db4b03484cb172e2
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-06-26 14:14:10 +0100

    made a more advanced product component for different rendering based on its parent context.

--- client/src/components/ContentPageProduct.jsx
+++ client/src/components/ContentPageProduct.jsx
@@ -1,16 +1,45 @@
 import React, {useEffect, useState} from 'react'
+import BuyButton from "./BuyButton";
 
+export default function ContentPageProduct( {product, view} ) {
 
-export default function ContentPageProduct( {product}) {
+    if (view === 'basic') {
+        return (
+            <a className="ProductModule" href={`/product/${product._id}`} >
+                <img src={product.image} alt="" />
 
-    return (
-        <a className="ProductModule" href={`/product/${product.id}`}>
-            <img src={product.image} alt="" />
+                <div className="productInfo">
+                    <div className="productCost">&euro;{product.price}</div>
+                    <div className="productTitle">{product.name}</div>
+                </div>
+            </a>
+        )
+    }
+    else if (view === 'advanced') {
+        console.log(product.image);
 
-            <div className="productInfo">
-                <div className="productTitle">{product.name}</div>
-                <div className="productCost">{product.price}</div>
+        return (
+            <div className="ProductModule">
+                <div className="productContent">
+                    <img src={product.image} alt="" />
+                    <div className="productInfo">
+                        <div className="productTitle">{product.name}</div>
+                        <div className="productCost">&euro;{product.price}</div>
+
+                        <div className="aboutProduct">
+                            <p>vv About this item vv</p>
+                            <div className="productDesc">{product.description}</div>
+                        </div>
+                        <div className="productStock">{product.stock} pcs in stock</div>
+                        <BuyButton/>
+                    </div>
+                </div>
             </div>
-        </a>
-    )
+        )
+    }
+    else {
+        return (
+            <p>null</p>
+        )
+    }
 }
