d60626d081f9624a08becae7dc500364bd48357d
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-06-29 22:46:40 +0100

    the actual page for each product, fetches the mongoose id from the parameter in the link and displays it. uses `product &&` to wait for the product before rendering to avoid any errors of unfound products

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import NavBar from "../components/NavBar";
+import ContentPageProduct from "../components/ContentPageProduct";
+import axios from 'axios';
+import {useParams} from "react-router-dom";
 
 export default function ProductPage() {
+
+    const { _id } = useParams();
+    let [product, setProduct]= useState(null);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/products/${_id}`)
+            .then(r => {
+                setProduct(r.data);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <div className="ProductPage">
-            <p>ProductPage</p>
+            <NavBar />
+            <hr />
+            {product && <ContentPageProduct product={product} view={'advanced'}/>}
         </div>
     )
 }
