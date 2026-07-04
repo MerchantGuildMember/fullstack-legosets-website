244f824d4c003091313df4eb310ed3ea9f9ebb43
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-04 10:10:00 +0100

    added a searchbar icon from react-icons

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
+import { FaSearch } from "react-icons/fa";
 
+export default function Searchbar({ search, setSearch }) {
 
-export default function Searchbar( {search, setSearch} ) {
-
-    return(
+    return (
         <div className="Searchbar">
-            <input className="navSearchBar"
-                   value = {search}
-                   onChange={(e) => setSearch(e.target.value)}
-                   placeholder="Search..." />
+            <div className="searchInputWrapper">
+                <FaSearch className="searchIcon" />
+                <input
+                    className="navSearchBar"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search..."
+                />
+            </div>
         </div>
-    )
+    );
 }
