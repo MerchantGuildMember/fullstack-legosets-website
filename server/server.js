a269f33cb0545fa0dc68139b9185021adcec033e
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-08 21:33:20 +0100

    two new router paths for users and carts

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const cors = require("cors");
 
 const productRouter = require("./routes/products");
+const userRouter = require("./routes/users");
+const cartRouter = require("./routes/cart");
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", productRouter);
+app.use("/", userRouter);
+app.use("/", cartRouter);
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
