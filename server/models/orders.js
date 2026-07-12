fca184143553f3c43a0ab08dea263619594c074d
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-12 06:05:50 +0100

    orders model updated to use the proper references

--- server/models/orders.js
+++ server/models/orders.js
@@ -3,14 +3,15 @@ const {Schema} = require("mongoose");
 
 let ordersSchema = new mongoose.Schema(
     {
-        userID: {type: Schema.Types.ObjectId, required: true},
-        productID: {type: Schema.Types.ObjectId, required: true},
+        userID: {type: Schema.Types.ObjectId, ref: 'users', required: true},
+        productID: {type: Schema.Types.ObjectId, ref: 'legosets', required: true},
         amount: {type: Number, required: true},
         address: {type: String, required: true},
         total_price: {type: Number, required: true},
     },
     {
-        collection: `orders`
+        collection: `orders`,
+        timestamps: true
     }
 )
 
