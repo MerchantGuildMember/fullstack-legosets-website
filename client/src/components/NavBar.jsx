eaac372fb016c2b774c7a00f2f78fe03821122f5
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-17 20:20:00 +0100

    handling isLoggedin status to render different texts

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Searchbar from "./Searchbar";
+import { MdAccountCircle, MdOutlineShoppingCart } from "react-icons/md";
 
-export default function NavBar( {search, setSearch } ) {
-    return(
+export default function NavBar( {search, setSearch, setIsLoggedIn, ...rest } ) {
+
+    const isLoggedIn = rest.isLoggedIn ?? false
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        delete localStorage.token
+        delete localStorage.name
+        delete localStorage.accessLevel
+        setIsLoggedIn(false)
+        navigate('/login')
+    }
+
+    return (
         <div className="Header">
             <ul className="nav">
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                {!isLoggedIn &&
+                    <li><Link to="/login">Log in</Link></li>
+                }
+                {isLoggedIn &&
+                    <li><button className="linkButton" onClick={handleLogout}>Log out</button></li>
+                }
             </ul>
             <Searchbar
                 search={search}
                 setSearch={setSearch}
             />
-            <div className="profileHolder">
-                <div className="miniPicture">
+            <div className="rightIcons">
+                <a className="shoppingCart" href="/cart">
+                    <MdOutlineShoppingCart color="white" size={48} />
+                </a>
 
-                </div>
-                <div className="chevron"></div>
+                <a className="profileHolder" href="/profile">
+                    <MdAccountCircle color="white" size={48} />
+                </a>
             </div>
         </div>
-
     )
 }
