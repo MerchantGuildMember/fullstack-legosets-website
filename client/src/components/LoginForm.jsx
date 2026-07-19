29ee89db5e445e13a3f02452cbd2cf88bcafcce4
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-19 12:36:15 +0100

    passing loggedinstatus to the login page, idea is to blue the login so users can opt to log out instead if tyhey are signed in

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,15 +1,26 @@
-export default function LoginForm() {
+export default function LoginForm( {handleSubmit, setEmail, setPassword, isLoggedIn } ) {
+
+    console.log(isLoggedIn)
     return (
         <div className="loginDiv">
             <h1 className="loginHeading">Welcome back</h1>
 
             <a href="/"> &lt;&lt; I've changed my mind, take me back &gt;&gt; </a>
 
-            <form className="loginForm">
-                <input type="text" className="email" placeholder="Email" />
-                <input type="password" className="password" placeholder="Password" />
-                <input type="submit" value="Login" />
-            </form>
+            {isLoggedIn &&
+                <form className="loginForm blur" onSubmit={handleSubmit}>
+                    <input type="text" className="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" className="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="submit" value="Login" />
+                </form>
+            }
+            {!isLoggedIn &&
+                <form className="loginForm" onSubmit={handleSubmit}>
+                    <input type="text" className="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" className="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="submit" value="Login" />
+                </form>
+            }
 
             <a href="/register"> &lt;&lt; Don't have an account? &gt;&gt; </a>
         </div>
