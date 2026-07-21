b2655af380b339584f1b3f3489f5cb4fad050585
Aleksy C <128327771+MerchantGuildMember@users.noreply.github.com>
2026-07-21 18:17:55 +0100

    login page signs user in and gives jwt

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import LoginForm from "../components/LoginForm";
-import Scroller from "../components/Scroller";
+import axios from "axios";
+import {SERVER_HOST} from "../config/global_constants"
+import { useNavigate } from "react-router-dom";
 
-export default function LoginPage() {
+export default function LoginPage({ isLoggedIn, setIsLoggedIn, ...props }) {
+
+    const [email, setEmail] = React.useState(props.email)
+    const [password, setPassword] = React.useState(props.password)
+    const navigate = useNavigate()
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        axios.post(`${SERVER_HOST}/users/login`, {email, password})
+            .then(res =>
+            {
+                localStorage.name = res.data.name
+                localStorage.accessLevel = res.data.accessLevel
+                localStorage.token = res.data.token
+                setIsLoggedIn(true)
+                navigate('/')
+            })
+            .catch(err => console.log(`${err.response.data}\n${err}`))
+    }
+
     return (
         <div className="LoginPage">
             <div className="loginDiv">
-                <LoginForm />
+                {!isLoggedIn && (
+                    <LoginForm
+                        handleSubmit={handleSubmit}
+                        setEmail={setEmail}
+                        setPassword={setPassword}
+                    />
+                )}
             </div>
         </div>
     )
